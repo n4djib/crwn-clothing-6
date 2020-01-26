6748d0e0ef7cf557dcc5b0abfb5cbd5e67f2a363
n4djib <n4djib@gmail.com>
2020-01-26 19:18:35 +0100

    implemented signup with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,34 @@ const config = {
     storageBucket: "crwn-db-2382b.appspot.com",
     messagingSenderId: "306563623716",
     appId: "1:306563623716:web:fb333ec69f06ac0a"
+};
+
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = userRef.get();
+
+    // creating the user if it doen't exist
+    if(!snapShot.exits) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+        
+    }
+
+    return userRef;
 }
 
 firebase.initializeApp(config);

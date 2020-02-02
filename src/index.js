a6899a90951d1ce107494e83046746e8b25feb15
n4djib <n4djib@gmail.com>
2020-02-02 01:27:28 +0100

    movrd directory and shop data to its own reducer, and created selectors and updated components

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
-import store from './redux/store.js'
+// import store from './redux/store.js'
+import {store, persistor} from './redux/store.js'
 
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
